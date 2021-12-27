c188224398843d0901789af283c56351256057d1
Rhys Crockett <rhysmorgancrockett@gmail.com>
2021-12-27 15:39:05 +0000

    Fixed the lotterychecker to now cycle through each row (ticket) from the list and each item (number) from the row. Then, match if the number apears from the winning number ticket in the generated ticket.

--- lotterychecker.py
+++ lotterychecker.py
@@ -1,6 +1,9 @@
 def findwinningnumbers(genticket, winningticket):
     matchingnumbers = []
-    for i in genticket:
-        if i in winningticket:
-            matchingnumbers.append(i)
+    for row in winningticket:
+        print(row)                                   # prints the individual rows (7 numbers of ticket)
+        for item in row:
+            print(item)                              # prints the individual items (numbers of ticket)
+            if item in genticket:
+                matchingnumbers.append(item)
     return matchingnumbers
