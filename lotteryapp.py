483229741024fc9f481ff4d54b9ddc557f2fa772
Rhys Crockett <rhysmorgancrockett@gmail.com>
2021-12-28 20:08:40 +0000

    Fixed some stuff to do with checker. Mainly, the program will now count each time a winning number is found in the generated ticket using the winning ticket for that day. Currently, a print statement is used if no matching number is found. This is useful for checking that numbers are found.

--- lotteryapp.py
+++ lotteryapp.py
@@ -14,12 +14,12 @@ readtime = Timer()
 readtime.start()
 # these datasets are lists
 dataset01 = winningnumbers("Lottery-numbers-csv/lotto-results-2001.csv")
-#dataset02 = winningnumbers("Lottery-numbers-csv/lotto-results-2002.csv")
-#dataset03 = winningnumbers("Lottery-numbers-csv/lotto-results-2003.csv")
+dataset02 = winningnumbers("Lottery-numbers-csv/lotto-results-2002.csv")
+dataset03 = winningnumbers("Lottery-numbers-csv/lotto-results-2003.csv")
 readtime.stop()
 
 print(myticket)
-#compare = [tuple(i) for i in dataset01] # tuple: ordered list immutable
+
 compare = [set(i) for i in dataset01] # set: unordered set unique
 #print(compare) # print the new set list
 
--- lotterychecker.py
+++ lotterychecker.py
@@ -2,12 +2,16 @@ def findwinningnumbers(genticket, dataset):
     matchingnumbers = []
     count = 0
     for row in dataset:                               # for each row (ticket) in the dataset
-        #print(row)                                   # prints the individual rows (7 numbers of ticket)
+        print(row)                                    # prints the individual rows (7 numbers of ticket)
         for item in row:                              # for each item in the row
             #print(item)                              # prints the individual items (numbers of ticket)
             if int(item) in genticket:                # check if the item (number: converted to int) is in generated ticket
-                matchingnumbers.append(item)          # if true, add to the matchingnumbers array
-                count = len(matchingnumbers)          # set count to the length of the array (counts how many numbers match)
-    print(f"Matching numbers: {count}")
-    return matchingnumbers
-    #return count
+                count += 1                            # increase the counter by 1
+                print(f"Matching numbers: {count}")   # print the count of matching numbers
+                matchingnumbers.append(count)         # add the count number to the matching numbers list
+            else:
+                count = 0
+                continue
+            #count = 0
+
+    return len(matchingnumbers)                       # return the length (numbers of) the array
