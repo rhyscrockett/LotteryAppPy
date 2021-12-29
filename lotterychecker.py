4421a77d8d824814ffab59e2090aa8b4cb1a10a1
Rhys Crockett <rhysmorgancrockett@gmail.com>
2021-12-29 16:08:35 +0000

    Fixed the checker to now count properly how many matching numbers were found per ticket. If not matching numbers are found, print statement. Once the checker has finished, print the total number of matching numbers found. Next: change the final matching number output to print how many 1,2,3,4,5,6,7 winning tickets were found.

--- lotterychecker.py
+++ lotterychecker.py
@@ -1,17 +1,26 @@
 def findwinningnumbers(genticket, dataset):
-    matchingnumbers = []
-    count = 0
-    for row in dataset:                               # for each row (ticket) in the dataset
-        print(row)                                    # prints the individual rows (7 numbers of ticket)
-        for item in row:                              # for each item in the row
-            #print(item)                              # prints the individual items (numbers of ticket)
-            if int(item) in genticket:                # check if the item (number: converted to int) is in generated ticket
-                count += 1                            # increase the counter by 1
-                print(f"Matching numbers: {count}")   # print the count of matching numbers
-                matchingnumbers.append(count)         # add the count number to the matching numbers list
+    matchingnumbers = []                                # list to hold items that appear in both tickets
+    totalmatchingnumbers = []                           # list that holds the number of total winning numbers
+    count = 0                                           # count variable holds the counter value
+    for row in dataset:                                 # for each row (ticket) in the dataset
+        print(row)                                      # prints the individual rows (7 numbers of ticket)
+        for item in row:                                # for each item in the row
+            #print(item)                                # prints the individual items (numbers of ticket)
+            if int(item) in genticket:                  # check if the item (number: converted to int) is in generated ticket
+                matchingnumbers.append(item)            # add the count number to the matching numbers list
+                print(f"Matching number found: {item}") # print the count of matching numbers
             else:
-                count = 0
-                continue
-            #count = 0
+                continue                                # if the number is not found, continue to the next number
 
-    return len(matchingnumbers)                       # return the length (numbers of) the array
+        if matchingnumbers:                             # if there is matching numbers in matchingnumbers
+            count = len(matchingnumbers)                # use the length of matchingnumbers list to store the count
+            print(f"(Counter: {count})")                # print the count for that ticket
+            totalmatchingnumbers.append(matchingnumbers)# add the numbers found for that ticket to another list
+            matchingnumbers.clear()                     # clear the matchingnumbers list for the next ticket
+            print("\n")                                 # new line
+        else:                                           # if no matchingnumbers are found in list
+            print("No Matching Numbers.\n")             # print statement
+            continue                                    # continue to next ticket
+        
+    print("\nTotal Matching Numbers: ")                 # print statement
+    return len(totalmatchingnumbers)                    # return the length (numbers of) the total dataset
