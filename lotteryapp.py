05c7b376b8fb8518699ce59c079568d52108ec5f
Rhys Crockett <rhysmorgancrockett@gmail.com>
2022-05-24 11:43:25 +0100

    Added main(), proper python standards.

--- lotteryapp.py
+++ lotteryapp.py
@@ -4,21 +4,25 @@ from lotterychecker import find_winning_numbers # needed for the checker functio
 from timer import Timer                       # needed for timing functions
 import os
 
-# Start timer and call the random function
-gentime = Timer()
-gentime.start()
-myticket = randomticket() # this is a set
-gentime.stop()
+def main():
+    """Main loop."""
+    # Start timer and call the random function
+    gentime = Timer()
+    gentime.start()
+    myticket = randomticket() # this is a set
+    print("Your ticket numbers:", myticket)
+    gentime.stop()
 
-print("Your ticket numbers:", myticket)
+    # Start another timer and call the reader function
+    readtime = Timer()
+    # main loop (for each file in dir, if filename ends with csv
+    for filename in os.listdir("Lottery-numbers-csv/"):
+        if filename.endswith(".csv"):
+            readtime.start()
+            # parse CSV file and return 
+            dataset = winningnumbers("Lottery-numbers-csv/" + filename)
+            find_winning_numbers(myticket, dataset)
+            readtime.stop()
 
-# Start another timer and call the reader function
-readtime = Timer()
-# main loop (for each file in dir, if filename ends with csv
-for filename in os.listdir("Lottery-numbers-csv/"):
-    if filename.endswith(".csv"):
-        readtime.start()
-        # parse CSV file and return 
-        dataset = winningnumbers("Lottery-numbers-csv/" + filename)
-        find_winning_numbers(myticket, dataset)
-        readtime.stop()
+if __name__ == '__main__':
+    main()
